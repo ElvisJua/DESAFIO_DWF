311e54bc3e82d0a4749ed75a0f11766960a545bc
ELVISRJ <81792600+ElvisJua@users.noreply.github.com>
2024-09-04 22:59:57 -0300

    SE SUBIOO

---
+++ src/main/java/sv/edu/udb/desafio2/dwf404_rojas_diego_ostorga_carlos_romero_elvis/dwf_rojas_diego_romero_elvis_ostorga_carlosdesafio2/HelloServlet.java
@@ -0,0 +1,28 @@
+package sv.edu.udb.desafio2.dwf404_rojas_diego_ostorga_carlos_romero_elvis.dwf_rojas_diego_romero_elvis_ostorga_carlosdesafio2;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
